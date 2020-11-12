8f934409ac9c765109cd0e00db0b61bdfb3464f7
Anders Hoffmann <55887624+AndersHoffmann@users.noreply.github.com>
2020-11-12 10:05:06 +0100

    Oprettet ConsoleTestEnv

---
+++ EstateBrokers/ConsoleTestEnv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTestEnv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Universe!");
+        }
+    }
+}
