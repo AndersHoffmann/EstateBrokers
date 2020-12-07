5e540c028533c2bfb9a2f76a37601afb7619e6b7
Anders Hoffmann <55887624+AndersHoffmann@users.noreply.github.com>
2020-12-07 14:47:14 +0100

    ContainerConfig

---
+++ EstateBrokers/GUI/ContainerConfig.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Controllers;
+using Presenters;
+using System;
+using System.Reflection;
+using UseCases;
+using UseCases.EvaluateProperty;
+
+namespace GUI
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainPage>();
+            builder.RegisterType<EvaluatePropertyController>().As<IEvaluatePropertyController>();
+            builder.RegisterType<EvaluateProperty>().As<IEvaluatePropertyInput>();
+            builder.RegisterType<EvaluatePropertyPresenter>().As<IEvaluatePropertyOutput>();
+            builder.RegisterType<PropertyValuationPage>().As<IEvaluatePropertyFrontEnd>();
+            
+
+            return builder.Build();
+        }
+    }
+}
