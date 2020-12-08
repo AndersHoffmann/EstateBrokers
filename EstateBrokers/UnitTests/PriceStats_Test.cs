6852674d3e4dcb930b7913e064245337e7ba75f3
Tobias Kristoffer Sørensen <Tobias_sorensen@outlook.dk>
2020-12-08 10:09:42 +0100

    Test

--- EstateBrokers/UnitTests/PriceStats_Test.cs
+++ EstateBrokers/UnitTests/PriceStats_Test.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Entities;
+using Gateways;
+using UseCases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using UseCases.OpenHouse;
 
 namespace UnitTests
 {
-    class PriceStats_Test
+    [TestClass]
+    public class PriceStats_Test
     {
+        [TestMethod()]
+        public void 
+        {
+            // Arrange
+
+
+            //Act
+
+
+
+            //Assert
+
+        }
+
     }
 }
