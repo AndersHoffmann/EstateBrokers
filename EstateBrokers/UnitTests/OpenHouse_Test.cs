b2f7d3eef52f3df1b245b65ee2b28055124e6102
Tobias Kristoffer Sørensen <Tobias_sorensen@outlook.dk>
2020-12-08 10:48:23 +0100

    Added Persistance to open house.

--- EstateBrokers/UnitTests/OpenHouse_Test.cs
+++ EstateBrokers/UnitTests/OpenHouse_Test.cs
@@ -13,6 +13,43 @@ namespace UnitTests
     [TestClass]
     public class OpenHouse_Test
     {
-        
+         List<Case> Cases = new List<Case>();
+         List<Realtor> realtors = new List<Realtor>();
+         
+
+        private void FillLists()
+        {
+            Cases.Clear();
+            realtors.Clear();
+
+            for (int i = 0; i < 18; i++)
+            {
+                Cases.Add(new Case());
+                Cases[i].CaseID = i;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                realtors.Add(new Realtor());
+                realtors[i].RealtorID = i;
+            }
+        }
+
+        [TestMethod()]
+        public void Number_Of_Properties_Should_Be_Equal()
+        {
+            //Arrange
+            FillLists();
+            OpenHouseLogic openHouseLogic = new OpenHouseLogic();
+
+            //Act
+            openHouseLogic.RunOpenHouse(realtors, Cases);
+
+            int realtorOne = Cases.Count(s => s.Realtor.RealtorID == 0);
+            int realtorTwo = Cases.Count(s => s.Realtor.RealtorID == 1);
+            int realtorThree = Cases.Count(s => s.Realtor.RealtorID == 2);
+
+            //Assert
+            Assert.AreEqual(realtorOne, realtorOne, realtorThree);
+        }
     }
 }
