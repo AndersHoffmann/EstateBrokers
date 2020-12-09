2617b1ea3547ed94735316212a44d2990fbec296
Anders Hoffmann <55887624+AndersHoffmann@users.noreply.github.com>
2020-12-09 13:11:58 +0100

    Started with delete Entry

---
+++ EstateBrokers/UseCases/DeleteEntry/IDeleteEntryInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UseCases.EvaluateProperty
+{
+    public interface IDeleteEntryInput
+    {
+        public void DeleteEntry(int caseID);
+
+    }
+}
